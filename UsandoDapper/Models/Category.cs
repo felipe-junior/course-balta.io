3f47a652ec66c713fd1d408739adb1695f8a549e
Felipe dos Santos <felipe-junior@users.noreply.github.com>
2022-03-19 01:41:46 -0300

    Tipando a consulta com um model

---
+++ UsandoDapper/Models/Category.cs
@@ -0,0 +1,8 @@
+namespace UsandoDapper.Models
+{
+    public class Category
+    {   
+        public Guid Id;
+        public string ?Title { get; set; }
+    }
+}
---
+++ UsandoDapper/Program.cs
@@ -0,0 +1,18 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using UsandoDapper.Models;
+
+const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$;Trusted_Connection=False; TrustServerCertificate=True;";
+
+using (var connection = new SqlConnection(connectionString)){
+    
+    connection.Open();
+    var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+
+    foreach (var category in categories)
+    {
+        System.Console.WriteLine($"{category.Id} - {category.Title}");
+    }
+
+}
+
